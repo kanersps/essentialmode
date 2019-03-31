76b50ee16012bc73fd2d817369c18aca86efdcf1
theslipyshow <43242850+theslipyshow@users.noreply.github.com>
2019-03-31 08:15:50 -0400

    Create updated file since old one was bugged.
    This will now save last known logout position to the database.

---
+++ client/main2.lua
@@ -0,0 +1,79 @@
+-- NO TOUCHY, IF SOMETHING IS WRONG CONTACT KANERSPS! --
+-- NO TOUCHY, IF SOMETHING IS WRONG CONTACT KANERSPS! --
+-- NO TOUCHY, IF SOMETHING IS WRONG CONTACT KANERSPS! --
+-- NO TOUCHY, IF SOMETHING IS WRONG CONTACT KANERSPS! --
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+		--if NetworkIsSessionStarted() then
+			--TriggerServerEvent('es:firstJoinProper')
+			--TriggerEvent('es:allowedToSpawn')
+			--return
+		--end
+	end
+end)
+
+local loaded = false
+local oldPos
+local pvpEnabled = true
+
+-- This code below was commented out and would make it not save last logout position ---
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1000)
+		local pos = GetEntityCoords(PlayerPedId())
+
+		if(oldPos ~= pos)then
+			TriggerServerEvent('es:updatePositions', pos.x, pos.y, pos.z)
+			oldPos = pos
+		end
+	end
+end)
+
+----------------------- END --------------------------------------
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		
+		if pvpEnabled then
+			for i = 0,32 do
+				if NetworkIsPlayerActive(i) then
+					SetCanAttackFriendly(GetPlayerPed(i), true, true)
+					NetworkSetFriendlyFireOption(true)
+				end
+			end
+		end
+	end
+end)
+
+local myDecorators = {}
+
+RegisterNetEvent("es:setPlayerDecorator")
+AddEventHandler("es:setPlayerDecorator", function(key, value, doNow)
+	myDecorators[key] = value
+	DecorRegister(key, 3)
+
+	if(doNow)then
+		DecorSetInt(PlayerPedId(), key, value)
+	end
+end)
+
+local enableNative = {}
+
+local firstSpawn = true
+AddEventHandler("playerSpawned", function()
+	for k,v in pairs(myDecorators)do
+		DecorSetInt(PlayerPedId(), k, v)
+	end
+
+	TriggerServerEvent('playerSpawn')
+end)
+
+RegisterNetEvent("es:enablePvp")
+AddEventHandler("es:enablePvp", function()
+	pvpEnabled = true
+end)
