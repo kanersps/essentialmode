d5620f14db32ed2cad5722a3a0bf6968fe9c6a1d
Kane <kanersps@pm.me>
2019-05-09 15:57:37 +0200

    Remove some dumb linebreaks

--- server/sqlite/sqlite.js
+++ server/sqlite/sqlite.js
@@ -6,8 +6,6 @@ const updateUser = (identifier, newData) => {
     for(key in newData){
         emit("es_sqlite:updateUserData", identifier, key, newData[key])
     }
-
-
 }
 
 on("es_sqlite:updateUser", updateUser)
