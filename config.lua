30aa8812a72f1cea8a8c8858728aa73d17197a2d
dutchplayers <info@dutch-players.nl>
2020-04-24 14:37:29 +0200

    Updated to work with latest ES_Exented

--- config.lua
+++
@@ -1,9 +0,0 @@
---       Licensed under: AGPLv3        --
---  GNU AFFERO GENERAL PUBLIC LICENSE  --
---     Version 3, 19 November 2007     --
-
-ip = GetConvar('es_couchdb_url', '127.0.0.1') 	                    -- Change to wherever your DB is hosted, use convar
-port = GetConvar('es_couchdb_port', '5984') 	                    -- Change to whatever port you have CouchDB running on, use convar
-auth = GetConvar('es_couchdb_password', 'root:1202') 	            -- "user:password", if you have auth setup, use convar
-metrics = GetConvar('es_enable_metrics', '0')                       -- Change to '0' to disable metrics, no identifiable data is stored
-show_zap = GetConvar('is_zap', '0')                                 -- Zaphosting check
